b68df6a009fdaccd7c16029f9647b9f301769fd2
Arthur Ryoo <jene0523@gmail.com>
2020-08-31 23:17:35 -0700

    Test Git clone

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
 {
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
     public class CommandsController : ControllerBase
     {
 
